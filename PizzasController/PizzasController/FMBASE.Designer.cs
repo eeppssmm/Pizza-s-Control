73ffcd09987b4b293db923b8c43ba2995e5fdfee
eeppssmm <edilsonpereiradesouza_melo@hotmail.com>
2014-10-15 01:12:24 -0300

    Criada Primeira tela do Projeto, essa tela da acesso a todos os Módulos do Sistema, o Padrão para nomeclatura dos arquivos é 1° digito inicial do modulo as 2 seguintes o tipo de arquivo e o restante algo que descreva a função da tela, exemplo BFMCADPES B= Básicos, FM= Form, CADPES= Cadastro de Pessoas.

---
+++ PizzasController/PizzasController/FMBASE.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PizzasController
+{
+    partial class FMBASE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FMBASE));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.básicosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroDeProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroDePessoasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estoqueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entradaDeProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saidaDeProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultaDeEstoqueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vendasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vendasToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.menuStrip1.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.básicosToolStripMenuItem,
+            this.estoqueToolStripMenuItem,
+            this.vendasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(717, 40);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // básicosToolStripMenuItem
+            // 
+            this.básicosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroDeProdutosToolStripMenuItem,
+            this.cadastroDePessoasToolStripMenuItem});
+            this.básicosToolStripMenuItem.Image = global::PizzasController.Properties.Resources._1413361994_note2_add;
+            this.básicosToolStripMenuItem.Name = "básicosToolStripMenuItem";
+            this.básicosToolStripMenuItem.Size = new System.Drawing.Size(100, 36);
+            this.básicosToolStripMenuItem.Text = "Básicos";
+            // 
+            // cadastroDeProdutosToolStripMenuItem
+            // 
+            this.cadastroDeProdutosToolStripMenuItem.Image = global::PizzasController.Properties.Resources.Box_Add_icon;
+            this.cadastroDeProdutosToolStripMenuItem.Name = "cadastroDeProdutosToolStripMenuItem";
+            this.cadastroDeProdutosToolStripMenuItem.Size = new System.Drawing.Size(233, 38);
+            this.cadastroDeProdutosToolStripMenuItem.Text = "Cadastro de Produtos";
+            // 
+            // cadastroDePessoasToolStripMenuItem
+            // 
+            this.cadastroDePessoasToolStripMenuItem.Image = global::PizzasController.Properties.Resources._1413361653_Add_Male_User;
+            this.cadastroDePessoasToolStripMenuItem.Name = "cadastroDePessoasToolStripMenuItem";
+            this.cadastroDePessoasToolStripMenuItem.Size = new System.Drawing.Size(233, 38);
+            this.cadastroDePessoasToolStripMenuItem.Text = "Cadastro de Pessoas";
+            // 
+            // estoqueToolStripMenuItem
+            // 
+            this.estoqueToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.entradaDeProdutosToolStripMenuItem,
+            this.saidaDeProdutosToolStripMenuItem,
+            this.consultaDeEstoqueToolStripMenuItem});
+            this.estoqueToolStripMenuItem.Image = global::PizzasController.Properties.Resources._1413359845_self1;
+            this.estoqueToolStripMenuItem.Name = "estoqueToolStripMenuItem";
+            this.estoqueToolStripMenuItem.Size = new System.Drawing.Size(104, 36);
+            this.estoqueToolStripMenuItem.Text = "Estoque";
+            // 
+            // entradaDeProdutosToolStripMenuItem
+            // 
+            this.entradaDeProdutosToolStripMenuItem.Image = global::PizzasController.Properties.Resources.Box_Add_icon;
+            this.entradaDeProdutosToolStripMenuItem.Name = "entradaDeProdutosToolStripMenuItem";
+            this.entradaDeProdutosToolStripMenuItem.Size = new System.Drawing.Size(225, 38);
+            this.entradaDeProdutosToolStripMenuItem.Text = "Entrada de Produtos";
+            // 
+            // saidaDeProdutosToolStripMenuItem
+            // 
+            this.saidaDeProdutosToolStripMenuItem.Image = global::PizzasController.Properties.Resources._1413362988_baggage_cart_box;
+            this.saidaDeProdutosToolStripMenuItem.Name = "saidaDeProdutosToolStripMenuItem";
+            this.saidaDeProdutosToolStripMenuItem.Size = new System.Drawing.Size(225, 38);
+            this.saidaDeProdutosToolStripMenuItem.Text = "Saida de Produtos";
+            // 
+            // consultaDeEstoqueToolStripMenuItem
+            // 
+            this.consultaDeEstoqueToolStripMenuItem.Image = global::PizzasController.Properties.Resources._1413363434_folder_search;
+            this.consultaDeEstoqueToolStripMenuItem.Name = "consultaDeEstoqueToolStripMenuItem";
+            this.consultaDeEstoqueToolStripMenuItem.Size = new System.Drawing.Size(225, 38);
+            this.consultaDeEstoqueToolStripMenuItem.Text = "Consulta de Estoque";
+            // 
+            // vendasToolStripMenuItem
+            // 
+            this.vendasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vendasToolStripMenuItem1});
+            this.vendasToolStripMenuItem.Image = global::PizzasController.Properties.Resources._1413362185_money;
+            this.vendasToolStripMenuItem.Name = "vendasToolStripMenuItem";
+            this.vendasToolStripMenuItem.Size = new System.Drawing.Size(114, 36);
+            this.vendasToolStripMenuItem.Text = "Comercial";
+            // 
+            // vendasToolStripMenuItem1
+            // 
+            this.vendasToolStripMenuItem1.Image = global::PizzasController.Properties.Resources._1413363097_Shoping_bag;
+            this.vendasToolStripMenuItem1.Name = "vendasToolStripMenuItem1";
+            this.vendasToolStripMenuItem1.Size = new System.Drawing.Size(168, 38);
+            this.vendasToolStripMenuItem1.Text = "Vendas";
+            // 
+            // FMBASE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 366);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FMBASE";
+            this.Text = "Pizza\'s Control";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem básicosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estoqueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vendasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroDeProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroDePessoasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entradaDeProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saidaDeProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultaDeEstoqueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vendasToolStripMenuItem1;
+    }
+}
+
